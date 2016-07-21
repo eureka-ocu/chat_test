5c329675db81398dd6c34fe426d6c80dbad4a051
nishitani yuki <yuki.nishitani85@gmail.com>
2016-07-21 23:22:07 +0900

    remove direnv and make production file secret instead

--- config/config.js
+++ config/config.js
@@ -1,23 +1,7 @@
-module.exports = {
-  development: {
-    username: process.env.DEV_USER_NAME,
-    password: JSON.parse(process.env.DEV_PASSWORD),
-    database: process.env.DEV_DATABASE,
-    host: process.env.DEV_HOST,
-    dialect: 'mysql'
-  },
-  test: {
-    username: process.env.TEST_USER_NAME,
-    password: JSON.parse(process.env.TEST_PASSWORD),
-    database: process.env.TEST_DATABASE,
-    host: process.env.TEST_HOST,
-    dialect: 'mysql'
-  },
-  production: {
-    username: process.env.PROD_USER_NAME,
-    password: JSON.parse(process.env.PROD_PASSWORD),
-    database: process.env.PROD_DATABASE,
-    host: process.env.PROD_HOST,
-    dialect: 'mysql'
-  }
-};
+const env = process.env.NODE_ENV;
+
+if (env) {
+  module.exports = require(`./config.${env}.js`);
+} else {
+  module.exports = require('./config.development.js');
+}
--- readme.md
+++ readme.md
@@ -1,16 +1,3 @@
 # chat test
 
 websocketでチャット
-
-## dependencies
-
-### [direnv](https://github.com/direnv/direnv)
-
-```
-$ brew install direnv
-$ cp _envrc .envrc
-$ vim .envrc
-// edit .envrc
-$ direnv allow
-// then some env variables are set
-```
